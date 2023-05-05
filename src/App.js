73c385522603cd225fb1f8a7a07f337372bccebb
BrianBFarias <92887307+BrianBFarias@users.noreply.github.com>
2023-05-05 13:52:54 -0400

    footer pt1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Cookies, CookiesProvider } from 'react-cookie';
 import SignUp from './components/pages/SignUp';
+import Services from './components/pages/Services';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/sign-up' element={<SignUp/>} />
+          <Route path='/services' element={<Services/>} />
         </Routes>
       </Router>
     </CookiesProvider>

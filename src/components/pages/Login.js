575011bcb35c5c0b736af73baa0f4dfe5f1e9126
BrianBFarias <92887307+BrianBFarias@users.noreply.github.com>
2023-05-07 11:52:25 -0400

    test2

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import '../../App.css';
+import './Login.css';
 
-export default function Login() {
-  return <h1 className='login'>Login</h1>;
+function AboutMe() {
+  return (
+    <div className="about-me">
+      <div className="avatar">
+        <img src={'./images/BrianPic.jpg'} alt="Avatar" width="350px" height="350px" />
+      </div>
+      <div className="info">
+        <h1>My Name</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut
+          commodo elit. Sed euismod nunc ut tortor elementum feugiat.
+        </p>
+      </div>
+    </div>
+  );
 }
+
+export default AboutMe;

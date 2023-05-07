6fe2a298406292f9c19ec0643ac90be58bfa4c57
BrianBFarias <92887307+BrianBFarias@users.noreply.github.com>
2023-05-07 12:00:34 -0400

    test3

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -5,7 +5,7 @@ function CardItem(props) {
   return (
     <>
       <li className='cards__item'>
-        <Link className='cards__item__link' to={props.path}>
+        <Link className='cards__item__link' to={props.path} target='blank'>
           <figure className='cards__item__pic-wrap' data-category={props.label}>
             <img className='cards__item__img' alt='Travel Image' src={props.src}/>
           </figure>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,32 +1,51 @@
-import React from 'react'
-import CardItem from './CardItem'
+import React from 'react';
 import './Cards.css';
+import CardItem from './CardItem';
 
 function Cards() {
   return (
     <div className='cards'>
-        <h1>Learn More!</h1>
-        <div className='cards__container'>
-            <div className='cards__wrapper'>
-                <ul className='cards__items'>
-                    <CardItem 
-                    src = "images/group-project.jpg"
-                    text = "Find the project that best fits you"
-                    path = '/services'
-                    />
-                  
-                    <CardItem 
-                    src = "images/Our-Story.jpg"
-                    text = "Our Story"
-                    // small label on group project picture VVVV
-                    label = "Founder/CFO"
-                    path = '/TeamInfo'
-                    />
-                </ul>
-            </div>
+      <h1>Projects I've worked on</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/AVL.jpg'
+              text=''
+              label='C++'
+              path='/services'
+            />
+            <CardItem
+              src='images/Schedule.jpg'
+              text=''
+              label='C++/SFML/Python'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/Pakudex.jpg'
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Java'
+              path='/services'
+            />
+            <CardItem
+              src='images/Cache.jpg'
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='C++'
+              path='/products'
+            />
+            <CardItem
+              src='images/PageRank.jpg'
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='C++'
+              path='/sign-up'
+            />
+          </ul>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;

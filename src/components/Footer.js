0fe241c10c862b5d65dd0ea1786249a1bc95077b
BrianBFarias <92887307+BrianBFarias@users.noreply.github.com>
2023-05-08 10:36:35 -0400

    mobile dev

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,52 @@
 import React from 'react';
 import './Footer.css';
 import { Link } from 'react-router-dom';
-import emailjs from "emailjs-com";
 import ContactUs from './contactForm.js';
-
-function sendEmail(e) {
-  e.preventDefault();
-  emailjs.sendForm('gmail', 'website_template', e.target, 'SHVR7fKbB9AkOyMWz')
-  .then((result) => {
-  console.log(result.text);
-  }, (error) => {
-  console.log(error.text);
-  });
-  e.target.reset()
-  }
 
 
 function Footer() {
+  const isMobile = /Mobi/.test(navigator.userAgent);
+
+  if(isMobile){
+    return (
+      <div className='mobile-footer-container'>
+        <section className='mobile-footer-subscription'>
+          <p className='mobile-heading'>
+            Contact me
+          </p>
+          <div className="mobile-container">
+            <ContactUs/>
+          </div>
+        </section>
+        <div class='mobile-footer-links'>
+        </div>
+        <section class='mobile-social-media'>
+          <div class='mobile-media-wrap'>
+            <small class='mobile-footer-notes'>Thanks for checking out my website</small>
+            <div class='mobile-social-icons'>
+              <Link
+                class='mobile-icon-link instagram'
+                to='https://www.instagram.com/brian.b.f/'
+                target='_blank'
+                aria-label='Instagram'
+              >
+                <i class='fab fa-instagram' />
+              </Link>
+  
+              <Link
+                class='mobile-icon-link LinkedIn'
+                to='https://www.linkedin.com/in/bbfarias/'
+                target='_blank'
+                aria-label='LinkedIn'
+              >
+                <i class='fab fa-linkedin' />
+              </Link>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
   return (
     <div className='footer-container'>
       <section className='footer-subscription'>
--- src/components/pages/Hobbies.js
+++ src/components/pages/Hobbies.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Hobbies.css';
 
 function Hobbies() {
+  const isMobile = /Mobi/.test(navigator.userAgent);
+
+  if (isMobile) {
+    return (
+      <div className="mobile-hobbies-container">
+        <div className="mobile-hobby">
+          <img src="./images/swim.jpg" alt="hobby1" />
+          <div>
+            <h2>Swimming</h2>
+            <p>During Highschool I was privleged with being captain of the swim team. I really enjoyed my years swimming and when I come back home sometimes ill 
+              swim a little just so I can make sure im still in athletic shape </p>
+          </div>
+        </div>
+        <div className="mobile-hobby">
+          <img src="./images/SW.jpg" alt="hobby2" />
+          <div>
+            <h2>Working out</h2>
+            <p>Since swimming I've picked up working out, where I go to UF's weightlifting gym 5 times a week, never skipping a day. Working out allows me to pipckup
+              on something I persistenetly work on while being able to socialize with friends </p>
+          </div>
+        </div>
+        <div className="mobile-hobby">
+          <img src="./images/js.jpg" alt="hobby3" />
+          <div>
+            <h2>New Side Skills</h2>
+            <p>I like to explore new environments as a relative "newby" to coding I feel it's important for me to explore different sectors of programmming, thus this website.
+              I've used this as not only an oppurtunity for an intro into full stack development but also working with API's and hosting platforms. Althought this website is simple
+              it is my first major project using Javascript and CSS paired with a emailjs API.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="hobbies-container">
       <div className="hobby">
@@ -31,7 +66,7 @@ function Hobbies() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 export default Hobbies;

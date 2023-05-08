5a8421ecc288b71aeaa04e567426d482340ef544
BrianBFarias <92887307+BrianBFarias@users.noreply.github.com>
2023-05-07 22:28:53 -0400

    DONEEE

--- src/components/pages/Hobbies.js
+++ src/components/pages/Hobbies.js
@@ -5,7 +5,7 @@ function Hobbies() {
   return (
     <div className="hobbies-container">
       <div className="hobby">
-        <img src="./images/sign-up.jpg" alt="hobby1" />
+        <img src="./images/swim.jpg" alt="hobby1" />
         <div>
           <h2>Swimming</h2>
           <p>During Highschool I was privleged with being captain of the swim team. I really enjoyed my years swimming and when I come back home sometimes ill 
@@ -13,18 +13,18 @@ function Hobbies() {
         </div>
       </div>
       <div className="hobby">
-        <img src="./images/sign-up.jpg" alt="hobby2" />
+        <img src="./images/SW.jpg" alt="hobby2" />
         <div>
           <h2>Working out</h2>
           <p>Since swimming I've picked up working out, where I go to UF's weightlifting gym 5 times a week, never skipping a day. Working out allows me to pipckup
-            on something I persistenetly work on while being able to social with friends </p>
+            on something I persistenetly work on while being able to socialize with friends </p>
         </div>
       </div>
       <div className="hobby">
-        <img src="./images/sign-up.jpg" alt="hobby3" />
+        <img src="./images/js.jpg" alt="hobby3" />
         <div>
           <h2>New Side Skills</h2>
-          <p>I like to explore new environments as a relative "newby" to coding i feel it's important for me to explore different sectors of programmming, thus this website.
+          <p>I like to explore new environments as a relative "newby" to coding I feel it's important for me to explore different sectors of programmming, thus this website.
             I've used this as not only an oppurtunity for an intro into full stack development but also working with API's and hosting platforms. Althought this website is simple
             it is my first major project using Javascript and CSS paired with a emailjs API.
           </p>

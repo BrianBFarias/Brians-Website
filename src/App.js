ddf14bf44daeca7b72363347794846784c29a6fb
BrianBFarias <92887307+BrianBFarias@users.noreply.github.com>
2023-05-07 22:16:33 -0400

    1 moreee

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Resume from './components/pages/Resume';
-import Services from './components/pages/Hobbies';
+import Hobbies from './components/pages/Hobbies';
 import Intro from './components/pages/Intro';
 
 
@@ -17,7 +17,7 @@ function App() {
           <Route path='/' element={<Home/>} />
           <Route path='/resume-pdf' element={<Resume/>} />
           <Route path='/intro' element={<Intro/>} />
-          <Route path='/services' element={<Services/>} />
+          <Route path='/hobbies' element={<Hobbies/>} />
         </Routes>
       </Router>
     </>
--- src/components/pages/Hobbies.js
+++ src/components/pages/Hobbies.js
@@ -1,6 +1,37 @@
 import React from 'react';
-import '../../App.css';
+import './Hobbies.css';
 
-export default function SignUp() {
-  return <h1 className='services'>What we offer</h1>;
+function Hobbies() {
+  return (
+    <div className="hobbies-container">
+      <div className="hobby">
+        <img src="./images/sign-up.jpg" alt="hobby1" />
+        <div>
+          <h2>Swimming</h2>
+          <p>During Highschool I was privleged with being captain of the swim team. I really enjoyed my years swimming and when I come back home sometimes ill 
+            swim a little just so I can make sure im still in athletic shape </p>
+        </div>
+      </div>
+      <div className="hobby">
+        <img src="./images/sign-up.jpg" alt="hobby2" />
+        <div>
+          <h2>Working out</h2>
+          <p>Since swimming I've picked up working out, where I go to UF's weightlifting gym 5 times a week, never skipping a day. Working out allows me to pipckup
+            on something I persistenetly work on while being able to social with friends </p>
+        </div>
+      </div>
+      <div className="hobby">
+        <img src="./images/sign-up.jpg" alt="hobby3" />
+        <div>
+          <h2>New Side Skills</h2>
+          <p>I like to explore new environments as a relative "newby" to coding i feel it's important for me to explore different sectors of programmming, thus this website.
+            I've used this as not only an oppurtunity for an intro into full stack development but also working with API's and hosting platforms. Althought this website is simple
+            it is my first major project using Javascript and CSS paired with a emailjs API.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Hobbies;

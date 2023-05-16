e39c72e02762a69071e42c6b8ba2bb288105abff
BrianBFarias <92887307+BrianBFarias@users.noreply.github.com>
2023-05-15 21:52:01 -0400

    welcome bug fix

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import '../App.css';
 import { Button } from './Button';
 import {Button1} from './Button1';
@@ -8,13 +8,24 @@ import Typewriter from 'typewriter-effect';
 function MainSection() {
   const isMobile = /Mobi/.test(navigator.userAgent);
 
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
   if(isMobile){
+    useEffect(() => {
+      const video = document.querySelector(".MobileVideo");
+      video.addEventListener("canplaythrough", () => {
+        setIsVideoLoaded(true);
+      });
+    }, []);
+  
     return (
       <div className='main-mobile-container'>
         <video className='MobileVideo' src="/images/Background.mp4" type="video/mp4" autoPlay playsInline loop muted></video> 
-        <div className = 'welcome-mobile'>
-          <h1>Welcome</h1>
+        {isVideoLoaded && (
+          <div className="welcome-mobile">
+            <h1>Welcome</h1>
           </div>
+          )}
           <br/>
         <div className='main-btns'>
           <Button
@@ -37,15 +48,24 @@ function MainSection() {
   }
   // const string = isMobile ? 'typewriter-container-mobile' : 'typewriter-container-web")';
 
+  useEffect(() => {
+    const video = document.querySelector(".WebVideo");
+    video.addEventListener("canplaythrough", () => {
+      setIsVideoLoaded(true);
+    });
+  }, []);
+
   return (
     <div className='main-container'>
       <video className = "WebVideo" autoplay = "autoplay" loop muted >
         <source src="/videos/main1.mp4" type="video/mp4">
         </source>
       </video>  
-      <div className = 'welcome-web'>
-        <h1>Welcome</h1>
+      {isVideoLoaded && (
+        <div className="welcome-web">
+          <h1>Welcome</h1>
         </div>
+      )}
         <br/>
       <div className='main-btns'>
         <Button

4e1544d0501e187247c0947731b05bd02a2ea991
Brian Farias <Brian03032003@gmail.com>
2023-08-07 19:35:10 -0400

    logo scroll

--- src/components/pages/Intro.js
+++ src/components/pages/Intro.js
@@ -41,34 +41,28 @@ function AboutMe() {
         <div className='skills-container'>
         <div id='skills'>
           <ul className='scroll'>
-            <li className='code'>C++</li>
-            <li className='code'>SQL</li>
-            <li className='code'>HTML</li>
-            <li className='code'>CSS</li>
-            <li className='code'>Javascript</li>
-            <li className='code'>Java</li>
-            <li className='code'>Python</li>
-            <li className='code'>Arm Assembly</li>
-            <li className='other'>Django</li>
-            <li className='other'>React</li>
-            <li className='other'>SFML</li>
-            <li className='other1'>DOM</li>
-            <li className='other1'>OOP</li>
+            <img src='/images/skills/C++.png' className='skill'/>
+            <img src='/images/skills/SQL.png' className='skill'/>
+            <img src='/images/skills/HTML.svg' className='skill'/>
+            <img src='/images/skills/CSS.png' className='skill'/>
+            <img src='/images/skills/JS.png' className='skill'/>
+            <img src='/images/skills/Java.png' className='skill'/>
+            <img src='/images/skills/Python.png' className='skill'/>
+            <img src='/images/skills/Django.png' className='skill'/>
+            <img src='/images/skills/React.png' className='skill'/>
+            <img src='/images/skills/SFML.png' className='skill'/>
           </ul>
           <ul className='scroll2'>
-            <li className='code'>C++</li>
-            <li className='code'>SQL</li>
-            <li className='code'>HTML</li>
-            <li className='code'>CSS</li>
-            <li className='code'>Javascript</li>
-            <li className='code'>Java</li>
-            <li className='code'>Python</li>
-            <li className='code'>Arm Assembly</li>
-            <li className='other'>Django</li>
-            <li className='other'>React</li>
-            <li className='other'>SFML</li>
-            <li className='other1'>DOM</li>
-            <li className='other1'>OOP</li>
+            <img src='/images/skills/C++.png' className='skill'/>
+            <img src='/images/skills/SQL.png' className='skill'/>
+            <img src='/images/skills/HTML.svg' className='skill'/>
+            <img src='/images/skills/CSS.png' className='skill'/>
+            <img src='/images/skills/JS.png' className='skill'/>
+            <img src='/images/skills/Java.png' className='skill'/>
+            <img src='/images/skills/Python.png' className='skill'/>
+            <img src='/images/skills/Django.png' className='skill'/>
+            <img src='/images/skills/React.png' className='skill'/>
+            <img src='/images/skills/SFML.png' className='skill'/>
           </ul>
         </div>
         </div>

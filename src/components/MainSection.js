a8967af3d7733c830bb5c53c1877bd00723d6c8e
Brian Farias <Brian03032003@gmail.com>
2023-11-23 00:19:01 -0500

    update

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -4,55 +4,105 @@ import './MainSection.css';
 import Typewriter from 'typewriter-effect';
 
 function MainSection() {
-  const isMobile = /Mobi/.test(navigator.userAgent);
-
-  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
-
-  if(isMobile){
-    useEffect(() => {
-      const video = document.querySelector(".MobileVideo");
-      video.addEventListener("canplaythrough", () => {
-        setIsVideoLoaded(true);
-      });
-    }, []);
+  var starFieldWidth = window.innerWidth;
+  var starFieldHeight = window.innerHeight*.45;
   
-    return (
-      <div className='main-mobile-container'>
-        <video className='MobileVideo' src="/images/Background.mp4" type="video/mp4" autoPlay playsInline loop muted></video> 
-        {isVideoLoaded && (
-          <><div className="welcome-mobile">
-            <Typewriter
-              options={{
-                strings: ["Hello, I'm Brian"],
-                autoStart: true,
-                loop: false,
-                delay: 100,
-                pauseFor: 999999
-              }}
-            />
-            </div></>
-          )}
-      </div>
-    );
+  useEffect(()=>{
+    addStars(starFieldWidth, starFieldHeight, 420);
+    animateStars(starFieldWidth, 5);
+    console.log("adding starts")
+  },[]);
+  
+  // TODO: make some stars bigger than others, and have some blue,
+// red or white
+function addStars(starFieldWidth, starFieldHeight, noOfStars) {
+  var starField = document.getElementById('star-field');
+  var numberOfStars = noOfStars;
+  
+  for (var i = 0; i < numberOfStars; i++) {
+    var star = document.createElement('div');
+  	star.className = 'star';
+    var topOffset = Math.floor((Math.random() * starFieldHeight) + 1);
+    var leftOffset = Math.floor((Math.random() * starFieldWidth) + 1);
+    star.style.top = topOffset + 'px';
+    star.style.left = leftOffset + 'px';
+    star.style.position = 'absolute';
+  	starField.appendChild(star);
   }
-  // const string = isMobile ? 'typewriter-container-mobile' : 'typewriter-container-web")';
+}
 
-  useEffect(() => {
-    const video = document.querySelector(".WebVideo");
-    video.addEventListener("canplaythrough", () => {
-      setIsVideoLoaded(true);
-    });
-  }, []);
+function animateStars(starFieldWidth, speed) {
+  var starField = document.getElementById('star-field');
+  var stars = starField.childNodes;
+
+  function getBobbingOffset(index) {
+    // Use sine function to create a bobbing effect
+    return Math.sin(index / 10) * 2; // You can adjust the amplitude (5 in this case) for the bobbing effect
+  }
+  
+  function getStarColor(index) {
+    if (index % 8 == 0)
+      return 'red';
+    else if (index % 10 == 0)
+      return 'yellow';
+    else if (index % 17 == 0)
+      return 'blue';
+    else
+      return 'white';
+  }
+  
+  function getStarDistance(index) {
+    if (index % 6 == 0)
+      return '';
+    else if (index % 9 == 0)
+      return 'near';
+    else if (index % 2 == 0)
+      return 'far';
+    else
+      return 0;
+  }
+  
+  function getStarRelativeSpeed(index) {
+    if (index % 6 == 0)
+      return 1;
+    else if (index % 9 == 0)
+      return 2;
+    else if (index % 2 == 0)
+      return -1;
+    else
+      return 0;
+  }
+  
+  setInterval(function() {
+    for (var i = 1; i < stars.length; i++) {
+      stars[i].className = 'star' + ' ' + getStarColor(i) +' '+ getStarDistance(i); 
+
+      var currentLeft = parseInt(stars[i].style.left, 10);
+      var leftChangeAmount = speed + getStarRelativeSpeed(i);
+      var leftDiff;
+      if (currentLeft - leftChangeAmount < 0) {
+        leftDiff = currentLeft - leftChangeAmount + starFieldWidth;
+      }
+      else {
+        leftDiff = currentLeft - leftChangeAmount;
+      }
+      stars[i].style.left = (leftDiff) + 'px';
+
+      var bobbingOffset = getBobbingOffset(i);
+      var topOffset = parseInt(stars[i].style.top, 10) + bobbingOffset;
+
+      stars[i].style.top = topOffset + 'px';
+
+    }
+    
+  }, 20);
+  
+}
 
   return (
-    <div className='main-container'>
-      <video className = "WebVideo" autoplay = "autoplay" loop muted >
-        <source src="/videos/main1.mp4" type="video/mp4">
-        </source>
-      </video>  
-      {isVideoLoaded && (
-        <><div className="welcome-web">
-          <Typewriter
+    <div id='star-field'>
+      <h1 id="name">            
+      <Typewriter
               options={{
                 strings: ["Hello, I'm Brian"],
                 autoStart: true,
@@ -60,9 +110,7 @@ function MainSection() {
                 delay: 100,
                 pauseFor: 999999
               }}
-            />
-          </div></>
-      )}
+            /></h1>
     </div>
   );
 }

fd88b6cd983fbede247d68bb04039ef9e8f2296f
Brian Farias <92887307+BrianBFarias@users.noreply.github.com>
2023-12-10 00:15:17 -0500

    updated build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1741a3377741ef6fbbf198f7f804bc19.js"
+  "/precache-manifest.bda00d31ab7d1b46677ba9c70b56116c.js"
 );
 
 workbox.clientsClaim();
